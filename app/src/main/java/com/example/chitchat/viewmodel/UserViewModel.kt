4bb99449f74fd0557be1f6111e7c0371297769a1
kartik1004 <kartiklive@gmail.com>
2019-09-27 19:44:42 +0530

    in work

--- app/src/main/java/com/example/chitchat/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/chitchat/viewmodel/UserViewModel.kt
@@ -1,15 +1,46 @@
 package com.example.chitchat.viewmodel
 
 import android.app.Application
+import android.util.Log
+import android.view.View
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.chitchat.services.model.UserModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
-class UserViewModel (application: Application):AndroidViewModel(application)
+class UserViewModel: ViewModel()
 {
 
-//    fun getUserList():String
-//    {
-//        return
-//    }
 
 
-}
+    open class getUsersList : LiveData<List<UserModel>>() {
+            override fun onActive() {
+                super.onActive()
+                var users = MutableLiveData<UserModel>()
+                FirebaseDatabase.getInstance().getReference("/users")
+                    .addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onCancelled(p0: DatabaseError) {
+
+                            Log.d("UserRepository", "databaseError ${p0.message}")
+                        }
+
+                        override fun onDataChange(p0: DataSnapshot) {
+                            if (p0.exists()) {
+
+
+                                Log.d("UserRepository", "UserList ${p0.children}")
+                            }
+                        }
+
+                    })
+
+            }
+        }
+    }
+
+
